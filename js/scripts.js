17ed88c9b1f7d8bd0f0b8d70b637e611e86669d4
svitorz <vitor.fabio.castro@gmail.com>
2023-06-19 17:58:56 -0300

    Finalização!
    Finalização do trabalho, correção dos ultimos textos para ingles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-// PRELOADE
-
 (function ($) {
     var counting = setInterval(function () {
         var loader = document.getElementById("percentage");
@@ -24,6 +13,7 @@
         loadscreen.style.width = Width + "%";
       }, 10);
     })(jQuery);
+    
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -69,3 +59,5 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+$('body').scrollspy({target: ".navbar"});
